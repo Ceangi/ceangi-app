9078954567ae6aab44a987902923c26b3ffd3dc9
Avila George <george.avila@external.bper.it>
2024-12-18 23:11:46 +0100

    errore

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/services/auth.service';
 import { Router } from '@angular/router';
 import { User } from 'src/models/user';
 import { UserService } from 'src/services/user.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +16,7 @@ export class LoginPage implements OnInit {
   errorMessage: string = '';
   user!: User;
 
-  constructor(private userService: UserService,private authService: AuthService, private router: Router) { }
+  constructor(private userService: UserService, private toastController: ToastController, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     console.log("ini");
@@ -23,20 +24,32 @@ export class LoginPage implements OnInit {
     if (this.user?.accessToken) {
       this.router.navigate(['/profile']);
     }
-   }
+  }
+
+  async presentToast(message: string, color: 'success' | 'danger') {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      color: color,
+      position: 'bottom',
+    });
+    toast.present();
+  }
+
 
   onSubmit() {
     if (this.email && this.password) {
       this.authService.login({ email: this.email, password: this.password }).subscribe(
         (response) => {
           console.log('Login successful:', response);
+          this.presentToast('Login eseguito con successo', 'success');
           localStorage.setItem('accessToken', response.accessToken);
           localStorage.setItem('user', JSON.stringify(response)); // Save user object as a string
           this.router.navigate(['/']); // Redirect to the home page after login
         },
         (error) => {
           console.error('Login error:', error);
-          this.errorMessage = 'Credenziali non valide. Riprova.';
+          this.presentToast('Errore ' + error, 'danger');
         }
       );
     } else {
